1b4eb260d1b183ef1dfc2437ee9a28ca6c3282d1
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-09-10 11:01:36 -0400

    Finished Week 1 Assignment

---
+++ week1/todolist/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div>
+      <h1>List of To Do's</h1>
+      <li>
+        <ul>Get Dressed</ul>
+        <ul>Take Brother to School</ul>
+        <ul>Get Started on schoolwork</ul>
+        <ul>Pick Brother up from School</ul>
+        <ul>Go to sleep</ul>
+      </li>
+    </div>
+  );
+}
+
+export default App;
