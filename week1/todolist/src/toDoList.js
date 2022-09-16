f54cccf63b834bfa29f64d8d028920fdb6e720c2
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-09-16 11:47:24 -0400

    Made Changes

---
+++ week1/todolist/src/toDoList.js
@@ -0,0 +1,15 @@
+function ToDoList() {
+
+    return (
+      <div>
+        <h1>List of To Do's</h1>
+        <ul>
+          <li>Get Dressed</li>
+          <li>Take Brother to School</li>
+          <li>Get Started on schoolwork</li>
+          <li>Pick Brother up from School</li>
+          <li>Go to sleep</li>
+        </ul>
+      </div>
+    );
+  }
